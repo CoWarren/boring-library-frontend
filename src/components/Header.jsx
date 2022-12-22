80280316a20bec9c1b85124598c583344abc493f
Obin Gold <individualog@gmail.com>
2022-12-22 14:44:58 -0500

    Better responsiveness for Header component, still incomplete for tablets. git push --set-upstream origin main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,53 @@
-import React,{useState} from 'react'
-import logoTBL from '../assets/TheBoringLibrary.png'
-import LoginBtn from './LoginBtn'
+import React, { useState } from "react";
+import logoTBL from "../assets/TheBoringLibrary.png";
+import LoginBtn from "./LoginBtn";
 
-const Header = ({setPage,btnText,setBtnText,page}) => {
-  const [username, setUsername] = useState('')
+const Header = ({ setPage, btnText, setBtnText, page }) => {
+  const [username, setUsername] = useState("");
 
-  function showUsername(){
-    
-
-    if (sessionStorage.getItem('userId') !== '-1' && page === 'main'){
-
-      fetch(`http://localhost:8080/account/${sessionStorage.getItem('userId')}`).then((req) => req.json()).then((data) => setUsername(data.username))
+  function showUsername() {
+    if (sessionStorage.getItem("userId") !== "-1" && page === "main") {
+      fetch(`http://localhost:8080/account/${sessionStorage.getItem("userId")}`)
+        .then((req) => req.json())
+        .then((data) => setUsername(data.username));
 
       return (
-        <div  className="username">
-          <h2 data-testid="header-username" >Logged in as: {username}</h2>
+        <div className="username-container">
+          <h2 data-testid="header-username">Logged in as: {username}</h2>
         </div>
-      )
-    } else if (page === 'main') {
+      );
+    } else if (page === "main") {
       return (
-        <div className="username">
+        <div className="username-container">
           <h2>Not Signed In</h2>
         </div>
-      )
+      );
     }
-    
   }
 
   return (
-    <header data-testid="header-test" className='flex-center-a'>
+    <header data-testid="header-test" className="flex-center-a">
       <div className="logo-container">
-        <img data-testid="header-img-test" src={logoTBL} alt="The Boring Libray Logo" onClick = {() => {setPage('main')}}/>
+        <img
+          data-testid="header-img-test"
+          src={logoTBL}
+          alt="The Boring Libray Logo"
+          onClick={() => {
+            setPage("main");
+          }}
+        />
       </div>
-      {showUsername()}
-      <LoginBtn page={page} setPage={setPage} btnText={btnText} setBtnText={setBtnText} />
+      <div className="user-interaction-ctrl-container flex">
+        {showUsername()}
+        <LoginBtn
+          page={page}
+          setPage={setPage}
+          btnText={btnText}
+          setBtnText={setBtnText}
+        />
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
