c96d75aad743369ed1d2d220dfb0b70e97280b3b
Obin Gold <individualog@gmail.com>
2022-12-22 22:49:07 -0500

    Added full responsiveness for general layouts, and component improvements. Warning on minor issues with version control.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import './App.scss';
-import Searchbar from './components/Searchbar';
-import Header from './components/Header';
-import Favourites from './components/Favourites'
-import {useState, useEffect} from 'react'
-import Login from './components/Login';
+import "./App.scss";
+import Searchbar from "./components/Searchbar";
+import Header from "./components/Header";
+import Favourites from "./components/Favourites";
+import { useState, useEffect } from "react";
+import Login from "./components/Login";
 
 // Began building at SearchBar first
 function App() {
+  const [updateFavourite, setUpdateFavourite] = useState(0);
+  const [page, setPage] = useState("main"); //page which is being displayed (login or main)
+  const [btnText, setBtnText] = useState("Login / Sign up"); //update text on login button
+  const [favorites, setFavorites] = useState([]);
 
-  const [updateFavourite, setUpdateFavourite] = useState(0)
-  const [page, setPage] = useState('main') //page which is being displayed (login or main)
-  const [btnText, setBtnText] = useState('Login / Sign up') //update text on login button
-  const [favorites, setFavorites] = useState([])
-  
-  let [bookPage,setBookPage] = useState(1) //current batch of books from api 
+  let [bookPage, setBookPage] = useState(1); //current batch of books from api
 
   if (page === "main") {
     // initial commitß
@@ -25,7 +24,7 @@ function App() {
           setBtnText={setBtnText}
           page={page}
         />
-        <main className='flex flex-wrap'>
+        <main>
           <Favourites
             updateFavourite={updateFavourite}
             setUpdateFavourite={setUpdateFavourite}
@@ -46,16 +45,8 @@ function App() {
   } else if (page === "login") {
     return (
       <div className="App flex-column">
-        <Header 
-          setPage={setPage} 
-          btnText={btnText} 
-          setBtnText={setBtnText}
-        />
-        <Login 
-          setPage={setPage} 
-          page={page} 
-          setBtnText={setBtnText}
-        />
+        <Header setPage={setPage} btnText={btnText} setBtnText={setBtnText} />
+        <Login setPage={setPage} page={page} setBtnText={setBtnText} />
       </div>
     );
   }
--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
-import {addToFavourites, removeFavourite} from "../Utilities/favouritesFunction"
+import {
+  addToFavourites,
+  removeFavourite,
+} from "../Utilities/favouritesFunction";
 
 const Favourites = ({
   updateFavourite,
@@ -12,8 +15,9 @@ const Favourites = ({
   const [readList, setReadList] = useState([]);
   const [filteredRead, setFilteredRead] = useState(false);
   const [filteredWishList, setFilteredWishList] = useState(false);
-  const [favSearch, setFavSearch] = useState([])
-  const [favSearchState, setFavSearchState] = useState(false)
+  const [favSearch, setFavSearch] = useState([]);
+  const [favSearchState, setFavSearchState] = useState(false);
+  const [open, setOpen] = useState(false);
   const [err, setErr] = useState("");
 
   useEffect(() => {
@@ -25,16 +29,26 @@ const Favourites = ({
 
     // two more api request one to remove from the database
     //(needs to be a route or this could work on the same route which would add if book is not in, and remove if book is in)
-    fetch(`http://localhost:8080/users/get/wishList/${sessionStorage.getItem("userId")}`, {method: "GET"})
-    .then((res) => res.json())
-    .then((data) => {
-      setWishList(data);
-    });
-    fetch(`http://localhost:8080/users/get/readList/${sessionStorage.getItem("userId")}`, {method: "GET"})
-    .then((res) => res.json())
-    .then((data) => {
-      setReadList(data);
-    });
+    fetch(
+      `http://localhost:8080/users/get/wishList/${sessionStorage.getItem(
+        "userId"
+      )}`,
+      { method: "GET" }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setWishList(data);
+      });
+    fetch(
+      `http://localhost:8080/users/get/readList/${sessionStorage.getItem(
+        "userId"
+      )}`,
+      { method: "GET" }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setReadList(data);
+      });
   }, [updateFavourite]); //(JSON ERROR)
 
   async function editWishList(bookId) {
@@ -74,103 +88,154 @@ const Favourites = ({
   }
 
   function handleReadFilter() {
-    if (filteredRead === true) {setFilteredRead(false)}
-    if (filteredRead === false){setFilteredRead(true)}
+    if (filteredRead === true) {
+      setFilteredRead(false);
+    }
+    if (filteredRead === false) {
+      setFilteredRead(true);
+    }
   }
 
   function handleWishListFilter() {
-    if (filteredWishList === true) {setFilteredWishList(false)}
-    if (filteredWishList === false){setFilteredWishList(true)}
+    if (filteredWishList === true) {
+      setFilteredWishList(false);
+    }
+    if (filteredWishList === false) {
+      setFilteredWishList(true);
+    }
   }
-  
-  function handleSearch(e){
-    e.preventDefault()
-    if (e.target[0].value === ""){
-      setFavSearchState(false)}
-    else{
-      fetch(`http://localhost:8080/users/${sessionStorage.getItem("userId")}/favourites/${e.target[0].value}`, {method: "GET"})
-      .then((res) => res.json())
-      .then((data) => {
-        setFavSearch(data.map(obj => obj.id));
-        setFavSearchState(true)
-      }
-    );
+
+  function handleSearch(e) {
+    e.preventDefault();
+    if (e.target[0].value === "") {
+      setFavSearchState(false);
+    } else {
+      fetch(
+        `http://localhost:8080/users/${sessionStorage.getItem(
+          "userId"
+        )}/favourites/${e.target[0].value}`,
+        { method: "GET" }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setFavSearch(data.map((obj) => obj.id));
+          setFavSearchState(true);
+        });
+    }
   }
-}
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
 
   return (
     <div className="fav-container flex-column">
-      <div className="fav-header">
-        <h1>Favourites</h1>
+      <div className="fav-header flex-column">
+        <div className="fav-header-title">
+          <h1>Favourites</h1>
+        </div>
+        <div className="fav-searchbar-container flex">
+          <div className="fav-searchbar-form-container flex">
+            <form
+              action=""
+              onSubmit={handleSearch}
+              className="favourite-searchbar-form flex"
+            >
+              <input
+                className="search-favs"
+                placeholder="Search through your favourites"
+              />
+              <button type="submit">Search</button>
+            </form>
+          </div>
 
-        <section>
-          <button
-          onClick={handleReadFilter}
-          >
-            { !filteredRead ? "filter books marked as read":"filtered by read" }
-          </button>
-          <button
-          onClick={handleWishListFilter}
-          >
-            {!filteredWishList? "filter books marked on wishlist":"filtered by wishlist"}
-          </button>
-        </section>
-        <form action="" onSubmit={handleSearch} className="favourite-filter">
-          <input
-            className="search-favs"
-            placeholder="Search through your favourites"
-          />
-          <button type="submit">search</button>
-        </form>
+          <div className="fav-form-filter-dropdown">
+            <button type="button" onClick={handleOpen}>
+              Filters
+            </button>
+            {open ? (
+              <ul className="menu">
+                <li className="menu-item">
+                  <button onClick={handleReadFilter}>
+                    {!filteredRead
+                      ? "filter books marked as read"
+                      : "filtered by read"}
+                  </button>
+                </li>
+                <li className="menu-item">
+                  <button onClick={handleWishListFilter}>
+                    {!filteredWishList
+                      ? "filter books marked on wishlist"
+                      : "filtered by wishlist"}
+                  </button>
+                </li>
+              </ul>
+            ) : null}
+            {/* {open ? <div>Is Open</div> : <div>Is Closed</div>} */}
+          </div>
+        </div>
       </div>
       <div className="fav-books flex">
-        
         {favorites.length === 0 && (
           <h2 className="errorText">No favourites to display</h2>
         )}
 
         {favorites.map(function (book) {
-          if(!filteredWishList || filteredWishList && wishList.includes(book.id)){
-            if (!filteredRead || filteredRead && readList.includes(book.id)){
-              if(!favSearchState || favSearchState && favSearch.includes(book.id)){
-              return (
-                <div key={book.id} className="book">
-                  <img src={book.thumbnail} />
-                  <h2>{book.title}</h2>
-                  <h2>{book.author}</h2>
-                  <section className="filter-buttons">
+          if (
+            !filteredWishList ||
+            (filteredWishList && wishList.includes(book.id))
+          ) {
+            if (!filteredRead || (filteredRead && readList.includes(book.id))) {
+              if (
+                !favSearchState ||
+                (favSearchState && favSearch.includes(book.id))
+              ) {
+                return (
+                  <div key={book.id} className="book">
+                    <img src={book.thumbnail} />
+                    <h2>{book.title}</h2>
+                    <h2>{book.author}</h2>
+                    <section className="filter-buttons">
+                      <button
+                        onClick={function () {
+                          editReadList(book.id);
+                        }}
+                      >
+                        {readList.includes(book.id)
+                          ? "remove from to read list"
+                          : "add to read list"}
+                      </button>
+                      <button
+                        onClick={function () {
+                          editWishList(book.id);
+                        }}
+                      >
+                        {wishList.includes(book.id)
+                          ? "remove from wish list"
+                          : "Add to wish list"}
+                      </button>
+                    </section>
                     <button
-                      onClick={function () {
-                        editReadList(book.id);
+                      className="fav-button"
+                      onClick={function (event) {
+                        removeFavourite(
+                          event,
+                          book.id,
+                          updateFavourite,
+                          setUpdateFavourite,
+                          favorites,
+                          setFavorites
+                        );
                       }}
                     >
-                    {readList.includes(book.id)
-                      ? "remove from to read list"
-                      : "add to read list"}
-                  </button>
-                  <button
-                    onClick={function () {
-                      editWishList(book.id);
-                    }}
-                  >
-                    {wishList.includes(book.id)
-                      ? "remove from wish list"
-                      : "Add to wish list"}
-                  </button>
-                </section>
-                <button className = 'fav-button'
-                  onClick={function (event) {
-                    removeFavourite(event,book.id,updateFavourite,
-                      setUpdateFavourite,
-                      favorites,
-                      setFavorites);
-                  }}
-                >
-                  REMOVE FROM FAVOURITES ❌
-                </button>
-              </div>
-            );}}}
-          })}
+                      REMOVE FROM FAVOURITES ❌
+                    </button>
+                  </div>
+                );
+              }
+            }
+          }
+        })}
       </div>
     </div>
   );
